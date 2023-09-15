6e2d041d3ac78aebf0832803a1b8eb034bec18c2
maxx964 <127957496+maxx964@users.noreply.github.com>
2023-09-15 16:05:33 +0300

    ....\\\\

--- js/dashboard.js
+++ js/dashboard.js
@@ -22,3 +22,101 @@ document.getElementById("calorie-form").addEventListener("submit", function (e) 
     document.getElementById("calories").textContent = `Необхідна кількість калорій: ${calories.toFixed(2)} ккал/день`;
     document.getElementById("result").style.display = "block";
 });
+
+// ========================================================================
+
+document.addEventListener("DOMContentLoaded", function () {
+  const mealButtons = document.querySelectorAll(".meal-button");
+  const mealLists = document.querySelectorAll(".meal-list");
+
+  mealButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const mealType = button.getAttribute("data-meal");
+
+      // Переключайте видимість списку страв в залежності від кнопки
+      mealLists.forEach((list) => {
+        list.style.display = list.id === `${mealType}-list` ? "block" : "none";
+      });
+    });
+  });
+});
+
+// =============================================================
+
+// Отримуємо кнопку "Сніданок" за її ідентифікатором
+var breakfastButton = document.getElementById('breakfastButton');
+
+// Отримуємо список страв за ідентифікатором
+var mealList = document.getElementById('mealList');
+
+// Додаємо обробник події для кнопки "Сніданок"
+breakfastButton.addEventListener('click', function () {
+  // Перевіряємо, чи список видимий
+  if (mealList.style.display === 'block') {
+    // Якщо так, то ховаємо список
+    mealList.style.display = 'none';
+  } else {
+    // Якщо ні, то показуємо список
+    mealList.style.display = 'block';
+  }
+});
+// 2================================================
+
+// Отримуємо кнопку "Сніданок" за її ідентифікатором
+var lunchButton = document.getElementById('lunchButton');
+
+// Отримуємо список страв за ідентифікатором
+var mealListTwo = document.getElementById('mealListTwo');
+
+// Додаємо обробник події для кнопки "Сніданок"
+lunchButton.addEventListener('click', function () {
+  // Перевіряємо, чи список видимий
+  if (mealListTwo.style.display === 'block') {
+    // Якщо так, то ховаємо список
+    mealListTwo.style.display = 'none';
+  } else {
+    // Якщо ні, то показуємо список
+    mealListTwo.style.display = 'block';
+  }
+});
+
+// 3================================================
+
+// Отримуємо кнопку "Сніданок" за її ідентифікатором
+var snackButton = document.getElementById('snackButton');
+
+// Отримуємо список страв за ідентифікатором
+var mealListThree = document.getElementById('mealListThree');
+
+// Додаємо обробник події для кнопки "Сніданок"
+snackButton.addEventListener('click', function () {
+  // Перевіряємо, чи список видимий
+  if (mealListThree.style.display === 'block') {
+    // Якщо так, то ховаємо список
+    mealListThree.style.display = 'none';
+  } else {
+    // Якщо ні, то показуємо список
+    mealListThree.style.display = 'block';
+  }
+});
+// 4================================================
+
+// Отримуємо кнопку "Сніданок" за її ідентифікатором
+var dinnerButton = document.getElementById('dinnerButton');
+
+// Отримуємо список страв за ідентифікатором
+var mealListFour = document.getElementById('mealListFour');
+
+// Додаємо обробник події для кнопки "Сніданок"
+dinnerButton.addEventListener('click', function () {
+  // Перевіряємо, чи список видимий
+  if (mealListFour.style.display === 'block') {
+    // Якщо так, то ховаємо список
+    mealListFour.style.display = 'none';
+  } else {
+    // Якщо ні, то показуємо список
+    mealListFour.style.display = 'block';
+  }
+});
+
+
